e0d809a00ea19fe8f2ee0686a43ff1ec178f384b
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-05-14 23:13:55 +0200

    Updates

--- precache-manifest.ba0c7846754a0a79e5fcf6293a57fc37.js
+++ precache-manifest.ba0c7846754a0a79e5fcf6293a57fc37.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97fdb87c890216299db1d821337cacee",
+    "revision": "a48839951b7075dc75956ee90de06a71",
     "url": "/movie-search-app/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-search-app/static/css/2.82cfac90.chunk.css"
   },
   {
-    "revision": "ad0eb29adc318dd4419d",
-    "url": "/movie-search-app/static/css/main.59be86a0.chunk.css"
+    "revision": "2e79de1d94b90e11a868",
+    "url": "/movie-search-app/static/css/main.cd1fd34c.chunk.css"
   },
   {
     "revision": "3c4cdfcbaeafff569bf8",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-search-app/static/js/2.288b8572.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ad0eb29adc318dd4419d",
+    "revision": "2e79de1d94b90e11a868",
     "url": "/movie-search-app/static/js/main.96d5f5ce.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-search-app/precache-manifest.e65452cb3e7219aafeb1834359ad18eb.js"
+  "/movie-search-app/precache-manifest.ba0c7846754a0a79e5fcf6293a57fc37.js"
 );
 
 self.addEventListener('message', (event) => {
