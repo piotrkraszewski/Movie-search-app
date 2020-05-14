f0cb9e27e90ed37f28bf916da4201e4abacb506d
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-05-14 13:23:09 +0200

    Updates

---
+++ precache-manifest.3bbf7e3c3fb7134c71a1db58236b30f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2eff48a0cf5f61fe56dd4d45fba2e3a",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "3c4cdfcbaeafff569bf8",
+    "url": "/movie-search-app/static/css/2.82cfac90.chunk.css"
+  },
+  {
+    "revision": "befd65fe149a62b61f38",
+    "url": "/movie-search-app/static/css/main.e842377d.chunk.css"
+  },
+  {
+    "revision": "3c4cdfcbaeafff569bf8",
+    "url": "/movie-search-app/static/js/2.288b8572.chunk.js"
+  },
+  {
+    "revision": "4950814700e0a8be78dbf39618ee90b5",
+    "url": "/movie-search-app/static/js/2.288b8572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befd65fe149a62b61f38",
+    "url": "/movie-search-app/static/js/main.ce1e8136.chunk.js"
+  },
+  {
+    "revision": "1000dbedcb372678c4bd",
+    "url": "/movie-search-app/static/js/runtime-main.05d4af49.js"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/movie-search-app/static/media/tmdb.27b65cb4.svg"
+  }
+]);
