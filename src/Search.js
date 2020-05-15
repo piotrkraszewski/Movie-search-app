5322d4d19da12abb6f7e08b9b176a401be9e5bcb
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-05-15 22:24:58 +0200

    search.css removed, styles transported to scsss

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react'
 import TMDBLogo from './images/tmdb.svg'
 import './styles/main.scss'
-import './Search.css'
 import Scroolbar from './Scroolbar/Scroolbar'
 
 
