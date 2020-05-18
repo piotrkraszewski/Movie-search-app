202da7edea31d678c2af7f3ecd3881cb2c50f295
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-05-18 21:08:39 +0200

    added browser router

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import "bootswatch/dist/darkly/bootstrap.min.css"
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
