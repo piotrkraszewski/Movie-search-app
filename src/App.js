c1583aac3d3c81070b3f0c047905367f63fc7837
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2021-02-18 15:01:59 +0100

    moves part of the urls to const to shorten urls in code and make it more readable. deleted async function in useEffect - EsLint shows error but code works so I will ignore it

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 import './styles/main.scss'
 import { Route, Switch, useLocation } from 'react-router-dom'
@@ -10,8 +11,12 @@ import StartPage from './AppFiles/StartPage'
 import Movie from './AppFiles/Movie'
 import BgGreen2 from './images/BgGreen2.jpg'
 
+const API_KEY = 'api_key=cfe422613b250f702980a3bbf9e90716'
+const BASE_API_URL = 'https://api.themoviedb.org'
+const BASE_IMG_URL = 'https://image.tmdb.org/t/p/'
+
+
 export default function App () {
-  
 // === Routes Data ===
   const location = useLocation()  // key to app routes
   const pathname = location.pathname
@@ -25,24 +30,24 @@ export default function App () {
 
 
 // ==== Fetch StartPage ====
-  // sets interseallar BgImage image on StarPage. Otherwise BgImage of searched movie
+  // sets intersteallar BgImage image on StarPage. Otherwise BgImage of searched movie
   const [backgroundIMG, setBackgroundIMG] = useState('https://image.tmdb.org/t/p/original/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg')
 
   const [suggestions, setSuggestions] = useState([])
   const [searchbarText, setSearchbarText] = useState('')
   
-  const startPageUrl = `https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716&language=en-US&page=1`
   
-    async function fetchStartPage() {
-      const response = await axios.get(startPageUrl)
-      const res = response.data.results
-      const popularMovies = res.map(a => [
-        a.original_title,
-        a.id,
-        `https://image.tmdb.org/t/p/w500${a.poster_path}`,
-      ])
-      setSuggestions(popularMovies)
-    }
+  async function fetchStartPage() {
+    const startPageUrl = `${BASE_API_URL}/3/movie/popular?${API_KEY}`
+    const response = await axios.get(startPageUrl)
+    const res = response.data.results
+    const popularMovies = res.map(a => [
+      a.original_title,
+      a.id,
+      `${BASE_IMG_URL}w500${a.poster_path}`,
+    ])
+    setSuggestions(popularMovies)
+  }
 
   // if search is empty on main page it displays start page 
   // loads at page starup because searchbarText === '' at start
@@ -57,14 +62,9 @@ export default function App () {
   const [movieID, setMovieID] = useState(getInitialMovieID())
   const [movieData, setMovieData] = useState({})
   
-  useEffect(() => {
-    const movieUrl = `https://api.themoviedb.org/3/movie/${movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
-    
-    async function fetchMovieFromApi () {
-      const res = await axios.get(movieUrl)
-      setMovieData(res.data)
-    }
-    fetchMovieFromApi()
+  useEffect(async () => {
+    const res = await axios.get(`${BASE_API_URL}/3/movie/${movieID}?&${API_KEY}`)
+    setMovieData(res.data)
   }, [movieID])
 // ==== END Fetch movie page ====
 
@@ -83,18 +83,16 @@ export default function App () {
   }
 
   const showResInSearchBar = (value)=>{
-    if (value.length === 0) {
-      setOldSearchbarText('')
-    }
+    if (value.length === 0) setOldSearchbarText('')
     if (value.length >= 1) {
-      let url = `https://api.themoviedb.org/3/search/movie?query=%${value}&api_key=cfe422613b250f702980a3bbf9e90716`
+      let url = `${BASE_API_URL}/3/search/movie?query=%${value}&${API_KEY}`
       axios.get(url).then(response => {
         const res = response.data.results
         const movies = res
           .map(a => [
             a.original_title,
             a.id,
-            `https://image.tmdb.org/t/p/w500${a.poster_path}`,
+            `${BASE_IMG_URL}w500${a.poster_path}`,
           ])
           .slice(0, sliceNumber)
         setSuggestions(movies)
@@ -130,7 +128,7 @@ export default function App () {
   }, [movieData])
 
   useEffect(() => {
-    console.log('searchbarText: ' + searchbarText)
+    console.log(`searchbarText: ${searchbarText}`)
   }, [searchbarText])
 
   useEffect(() => {
@@ -171,7 +169,7 @@ export default function App () {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{duration: 1.5}}
-          src={backgroundIMG !== 'https://image.tmdb.org/t/p/originalnull' ? backgroundIMG : BgGreen2}
+          src={backgroundIMG !== `${BASE_IMG_URL}originalnull` ? backgroundIMG : BgGreen2}
           key={backgroundIMG}
           className='BgImage'
         />
