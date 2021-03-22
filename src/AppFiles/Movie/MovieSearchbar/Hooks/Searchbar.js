293b410e84fec40b99183b87fcbcd6fa7f15853d
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2021-03-22 13:41:40 +0100

    added X to clean searchbar results

--- src/AppFiles/Movie/MovieSearchbar/Hooks/Searchbar.js
+++ src/AppFiles/Movie/MovieSearchbar/Hooks/Searchbar.js
@@ -1,21 +1,32 @@
 import { useContext } from 'react'
 import 'styles/main.scss'
 import { AppContext } from 'AppFiles/Contexts/AppContext'
+import closeImg  from 'images/close.svg'
 
 export default function Searchbar(props) {
-  const {searchbarText, oldSearchbarText, onSearchbarTextChanging} = useContext(AppContext)
+  const {searchbarText, setSearchbarText, setOldSearchbarText,  oldSearchbarText, onSearchbarTextChanging} = useContext(AppContext)
 
   const {onClick, onKeyPress} = props
+
+  function resetSearch() {
+    setSearchbarText('')
+    setOldSearchbarText('')
+  }
 
   return (
     <form onSubmit={e => e.preventDefault()}>
       <input
         onChange={onSearchbarTextChanging}
         type='text'
-        placeholder='Search Movie Title...'
+        placeholder='Search Movie'
         value={searchbarText !== '' ? searchbarText : oldSearchbarText}
         onKeyPress={onKeyPress}
         onClick={onClick}
+      />
+      <img 
+        src={closeImg} 
+        onClick={resetSearch}
+        alt="close search"
       />
     </form>
   )
