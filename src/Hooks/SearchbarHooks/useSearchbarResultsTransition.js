261da0fd505fce859ba55debdff65cb404825fc6
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2021-04-16 20:16:31 +0200

    Fix noResults instantly disappearing without transition when we searched for something that exists

--- src/Hooks/SearchbarHooks/useSearchbarResultsTransition.js
+++ src/Hooks/SearchbarHooks/useSearchbarResultsTransition.js
@@ -16,10 +16,8 @@ export default function UseSearchbarResultsTransition (Component) {
       exit={{ opacity: 0 }}
       transition={{ 
         // turnary showQuickSearchRes makse results show instantly after clicking searchbar. 
-        // suggestions.length > 0 speeds showing when we type first letter in searchbar
         // normally we would have QUICK_SEARCH_TRANSITION delay 
-        duration: (showQuickSearchRes && suggestions.length > 0)
-                  ? QUICK_SEARCH_TRANSITION : 0, 
+        duration: showQuickSearchRes ? QUICK_SEARCH_TRANSITION : 0, 
         ease: 'easeInOut' 
       }}
     >
